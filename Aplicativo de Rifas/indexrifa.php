09f54942c3c25dbb828fdaa5a780fc54b6f740ad
lucasbguima <lucasbrandaoguimaraes@gmail.com>
2020-09-03 16:31:31 -0300

    Beta 0.1

--- Aplicativo de Rifas/indexrifa.php
+++ Aplicativo de Rifas/indexrifa.php
@@ -63,7 +63,7 @@ input:focus, textarea:focus, select:focus{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rifa</title>
-    <link rel="stylesheet" type="text/css" href="rifastyle.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="rifastyle.css"/>
 </head>
 <body>
   <div id="formrifa">
--- Aplicativo de Rifas/postrifa.php
+++ Aplicativo de Rifas/postrifa.php
@@ -1,71 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<style>
-* {
-  padding: 0;
-  margin: 0;
-  border: 0;
-  outline: none;
-
-}
-
-/* Chrome, Safari, Edge, Opera */
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-/* Firefox */
-input[type=number] {
-  -moz-appearance: textfield;
-}
-
-input:focus, textarea:focus, select:focus{
-        outline: none;
-    }
-
-#but{
-  padding: 5px;
-}
-
-#but:hover {
-  background-color: rgb(135,206,250);
-  cursor: pointer;
-}
-
-#gridrifa{
-  display: grid;
-  grid-template-columns: repeat(20, 1fr [col-start]);
-  grid-template-rows: repeat(50, 30px [col-start]);
-  
-}
-#inputn{
-  padding: 20px;
-  margin: 20px;
-  border: 1px solid;
-  font-size: 20px;
-}
-
-#inputc{
-  font-size: 20px;
-  padding: 20px;
-  border-radius: 10px;
-  background-color: rgb(135,206,250);
-
-}
-#inputc:hover{
-  background-color: rgb(0,191,255);
-}
-a{
-  all:unset;
-}
-</style>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rifa</title>
-    <link rel="stylesheet" type="text/css" href="rifastyle.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="rifastyle.css"/>
 </head>
 <body>
   <div id="formrifa">
