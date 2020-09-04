71cd59f4d7ed725b66623b64c30dcbe1697fb098
lucasbguima <lucasbrandaoguimaraes@gmail.com>
2020-09-04 14:32:15 -0300

    Btea 0.1

--- Aplicativo Estokar/estokar.py
+++ Aplicativo Estokar/estokar.py
@@ -1,24 +1,86 @@
+"""Bibliotecas"""
+import pandas as pd
+
+"""Variaveis Globais"""
+x = 1
+data = pd.DataFrame({
+    "Produto": [],
+    "Quantidade": [],
+    "Valor": []
+})
+
+"""Função Verificar Input"""
+def verificar():
+    if input==
+
+"""Função Retornar"""
+def retornar():
+    d = int(input("Deseja realizar outra operação? (1)Sim (2)Não "))
+    if d==1:
+        principal()
+    elif d==2:
+        d = int(input("Deseja sair e salvar seu estoque? (1)Sim (2)Não "))
+
+        if d==1:
+            print("\nVocê saiu e salvou seu estoque!\n")
+        elif d==2:
+            print("\nVocê saiu sem salvar seu estoque!\n")
+            return
+        else:
+            print("Erro!")
+            retornar()
+    else:
+        print("Erro!")
+        retornar()
+"""Função Cabecario"""
 def cabec():
     print("=========================================================")
 
-def my_function():
+"""Função Cadastrar"""
+def cadastrar():
+    global x
+    print("Bem vindo ao Cadastro de Produtos!\n")
+    n = input("Digite o nome do produto: ")
+    q = format(int(input("Digite a quantidade do produto: ")), '.0f')
+    v = format(float(input("Digite o valor unitário do produto: R$")), '.2f')
+
+    data.loc[x] = [n, q, "R$"+v]
+
+    d = int(input("Deseja cadastrar mais produtos? (1)Sim (2)Não "))
+
+    if d==1:
+        x = x+1
+        cadastrar()
+    elif d==2:
+        x = x+1
+        return x
+    else:
+        x = x+1
+        return x
+
+"""Função Principal"""
+def principal():
     cabec()
-    print("Bem vindo ao Aplicativo Estokar!\n")
+    print("Bem vindo ao Estokar!\n")
     print("Digite o ambiente de acesso:\n\n(1)Cadastro de produtos\n(2)Ver produtos\n(3)Remover Produtos\n")
 
     modo = int(input("Escolha um ambiente: "))
     
     if modo == 1:
         """CADASTRO DE PRODUTOS"""
-        print("\nBem vindo a tela de cadastro de produtos!\n")
-        nomeproduto = input("Digite o nome do produto: ")
-        qtdproduto = int(input("Digite a quantidade do produto: "))
-        valorproduto = format(float(input("Digite o valor unitário do produto: R$")), '.2f')
+        cadastrar()
+        print(data)
+        retornar()
 
     elif modo == 2:
         """TELA DE PRODUTOS"""
         print("\nBem vindo a tela de produtos!\n")
-    
+        if data.empty:
+             print('Você não tem nenhum produto cadastrado!')
+             retornar()
+        else:
+            print(data)
+            retornar()
    
     elif modo == 3:
         """REMOÇÃO DE PRODUTOS"""
@@ -27,9 +89,7 @@ def my_function():
     else:
         """ERRO"""
         print("Erro!")
-        my_function()
+        principal()
 
 """Chamada de Funções"""
-my_function()
-
-
+principal()
--- Aplicativo Estokar/teste.py
+++ Aplicativo Estokar/teste.py
@@ -1,6 +1,27 @@
 import pandas as pd
-import numpy as np
 
-notas = pd.Series([1,2,3,4,5], index=["Wilfred", "Abbie", "Harry", "Julia", "Carrie"])
-notas.describe()
+data = pd.DataFrame({
+    "Produto": [],
+    "Quantidade": [],
+    "Valor": []
+})
 
+def cadastrar():
+    global x
+    n = input("Digite o nome do produto: ")
+    q = format(int(input("Digite a quantidade do produto: ")), '.0f')
+    v = format(float(input("Digite o valor unitário do produto: R$")), '.2f')
+
+    data.loc[x] = [n, q, "R$"+v]
+
+    d = int(input("Deseja cadastrar mais produtos? (1)Sim (2)Não "))
+
+    if d==1:
+        x = x+1
+        cadastrar()
+    elif d==2:
+        return
+
+x = 1
+cadastrar()
+data
